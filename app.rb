ea0892249783ffe928fe72db5f881d907c68a9ec
Victoria B <viktorija.bourassa@gmail.com>
2019-02-26 06:02:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,9 +3,6 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    <p>97 Bowling Alley Lane, Boulder CO </p>
-    <h2>Bet You Can"'"t Bowl A Strike Every Time</h2>
-    <h1>Buster"'"s Best Bowling Alley</h1>
     status = 200
   end
 

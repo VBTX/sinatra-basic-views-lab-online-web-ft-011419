4605e5d1ee9319d2a6e0cfd78a357a818a0012b9
Victoria B <viktorija.bourassa@gmail.com>
2019-02-26 06:01:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ class App < Sinatra::Base
     <p>97 Bowling Alley Lane, Boulder CO </p>
     <h2>Bet You Can"'"t Bowl A Strike Every Time</h2>
     <h1>Buster"'"s Best Bowling Alley</h1>
+    status = 200
   end
 
   get "/index" do
